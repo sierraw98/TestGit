b12d9e4600b02978845842dd9c1e3746e8899dcd
Sierra <swill98@knights.ucf.edu>
2018-09-21 22:37:05 -0400

    Challenge 1, Tiers 1 and 2

---
+++ Roll A Ball Challenge/Assets/scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text scoreText;
+
+    private Rigidbody rb;
+    private int count;
+    private int score;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        score = 0;
+        SetScoreText();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+            score = score + 1;
+            SetScoreText();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            score = score - 1;
+            SetScoreText();
+        }
+        else if (count == 12)
+        {
+            transform.position = new Vector3(20.0f, 0.0f, 3.0f);
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You won with game with a score of: " + score.ToString();
+        }
+    }
+    void SetScoreText ()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
